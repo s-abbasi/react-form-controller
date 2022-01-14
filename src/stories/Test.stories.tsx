65201f3e6b7fab3e517ae86160d07138be19d3b5
sajad abbasi <s.abbasi@me.com>
2022-01-14 13:39:56 +0330

    basic setup

---
+++ src/stories/Test.stories.tsx
@@ -0,0 +1,8 @@
+import { storiesOf } from '@storybook/react';
+import { Test } from '../components/Test';
+
+const stories = storiesOf('App Test', module);
+
+stories.add('App', () => {
+  return <Test />;
+});

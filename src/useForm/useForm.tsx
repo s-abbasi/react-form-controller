6c53b5b88e47f6a3583ec5427c4e79c3c55b65f4
Sajad Abbasi <s.abbasi@me.com>
2022-03-16 19:36:21 +0330

    feat: add input bindings of type text (#52)
    closes #52

--- src/useForm/useForm.tsx
+++ src/useForm/useForm.tsx
@@ -1,3 +1,51 @@
-export const UseForm = (): JSX.Element => {
-    return <>useForm</>;
+/* eslint-disable no-console */
+import { mapObjIndexed } from 'ramda';
+import { setDefaultValue } from './useForm.helper';
+import {
+    ControlConvertor,
+    FormGroup,
+    FormModel,
+    GenerateBinding,
+    Observer,
+} from './useForm.types';
+
+const generateBinding: GenerateBinding = (model) => {
+    const observers: Observer[] = [];
+
+    const bind: ReturnType<GenerateBinding>['bind'] = (controlName) => ({
+        defaultValue: setDefaultValue(model[controlName]),
+        onChange: (ev) => {
+            const arg = { controlName, value: ev.target.value };
+            observers[0](arg);
+        },
+    });
+
+    const onFormChange: ReturnType<GenerateBinding>['onFormChange'] = (fn) => {
+        observers.push(fn);
+    };
+
+    return { bind, onFormChange };
+};
+
+const proxyHandler = {};
+
+const controlConvertor: ControlConvertor = (value) => {
+    return {
+        value: setDefaultValue(value),
+    };
+};
+
+export const useForm = (model: FormModel): FormGroup => {
+    const { bind, onFormChange } = generateBinding(model);
+    const controls = mapObjIndexed(controlConvertor, model);
+
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    const formGroup: FormGroup = { bind, ...controls };
+
+    onFormChange(({ controlName, value }): void => {
+        formGroup[controlName].value = value; // WARNING: this line mutates formGroup
+    });
+
+    return new Proxy(formGroup, proxyHandler);
 };
--- src/useForm/useForm.types.ts
+++ src/useForm/useForm.types.ts
@@ -1,55 +1,85 @@
-export type ValueType = string | number | boolean;
+import { ChangeEvent, HTMLAttributes } from 'react';
+
+export type NativeValueTypes = HTMLAttributes<HTMLInputElement>['defaultValue'];
 
 type ValidatorName = 'minValue' | 'maxValue' | 'minLength' | 'maxLength' | string;
 
 interface Validator {
     name: ValidatorName;
-    validateWith: (value: ValueType) => boolean;
+    validateWith: (value: NativeValueTypes) => boolean;
     message?: string;
 }
 
 export interface ControlModel {
-    defaultValue: ValueType;
+    defaultValue: NativeValueTypes;
     validators?: Validator[];
 }
 
-export interface FormModel {
-    [key: string | number]: ValueType | ControlModel;
+export type FormModel = Record<string, NativeValueTypes | ControlModel>;
+
+// interface ControlError {
+//     name: ValidatorName;
+//     message: Validator['message'];
+// }
+
+export interface JSXBinding {
+    onChange: (ev: ChangeEvent<HTMLInputElement>) => void;
+    defaultValue: NativeValueTypes;
 }
 
-interface ControlError {
-    name: ValidatorName;
-    message: Validator['message'];
+type Bind = (controlName: string) => JSXBinding;
+
+export type GenerateBinding = (model: FormModel) => {
+    bind: Bind;
+    onFormChange: (fn: Observer) => void;
+};
+
+export type Observer = (ev: { controlName: string; value: NativeValueTypes }) => unknown;
+
+// interface CtrlAddRemoveResult {
+//     success: boolean;
+//     message: string;
+// }
+
+export type ControlConvertor = (
+    value: NativeValueTypes | ControlModel,
+    key: string
+) => { value: NativeValueTypes };
+
+export interface Controls {
+    [key: string]: Control;
 }
 
-interface Control {
-    value: ValueType;
-    setValue: (value: ValueType) => void;
-    isValid: boolean;
-    isEnabled: boolean;
-    errors: ControlError;
-    reset: () => void;
-    addValidator: (validator: Validator) => void;
-    removeValidator: (name: Validator['name']) => void;
-    subscribe: (value: ValueType) => void;
-    enable: () => boolean;
-    disable: () => boolean;
-    dirty: boolean; // gets change event
-    touched: boolean; // gets blur event
+export interface Control {
+    value: NativeValueTypes;
+    // setValue: (value: ValueType) => void;
+    // isValid: boolean;
+    // isEnabled: boolean;
+    // errors: ControlError;
+    // reset: () => void;
+    // addValidator: (validator: Validator) => void;
+    // removeValidator: (name: Validator['name']) => void;
+    // subscribe: (value: ValueType) => void;
+    // enable: () => boolean;
+    // disable: () => boolean;
+    // isDirty: boolean; // gets change event
+    // isTouched: boolean; // gets blur event
 }
 
-interface CtrlAddRemoveResult {
-    success: boolean;
-    message: string;
-}
-
-export type FormGroup<T> = {
-    [Property in keyof T]: Control;
-} & {
+export type FormGroup = {
     [key: string]: Control;
 } & {
-    addControl: (validator: Validator) => CtrlAddRemoveResult;
-    removeControl: (validatorName: ValidatorName) => CtrlAddRemoveResult;
-    validate: () => void;
-    isTouchedAndValid: boolean;
+    bind: Bind;
 };
+
+// export type FormGroup<T> = {
+//     [Property in keyof T]: Control;
+// } & {
+//     [key: string]: Control;
+// } & {
+//     bind: Bind;
+//     addControl: (validator: Validator) => CtrlAddRemoveResult;
+//     removeControl: (validatorName: ValidatorName) => CtrlAddRemoveResult;
+//     validate: () => void;
+//     isTouchedAndValid: boolean;
+// };
